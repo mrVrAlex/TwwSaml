e505ff3309f8e9dc57a87884d943c99ac7f97708
Benjamin Nolan <ben.nolan@gmail.com>
2014-06-09 10:27:45 +0100

    Path corrections

--- src/TwwSaml/Module.php
+++ src/TwwSaml/Module.php
@@ -7,13 +7,13 @@ class Module
 
     public function onBootstrap(MvcEvent $e)
     {
-        define("TOOLKIT_PATH", __DIR__ . '/../../vendor/onelogin/php-saml/');
+        define("TOOLKIT_PATH", __DIR__ . '/../../../../vendor/onelogin/php-saml/');
         require_once(TOOLKIT_PATH . '_toolkit_loader.php');
     }
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../config/module.config.php';
     }
 
     public function getAutoloaderConfig()
