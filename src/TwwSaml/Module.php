06a63d50a9dfab8350e6153dbe8f09191f6b154c
Benjamin Nolan <ben.nolan@gmail.com>
2014-06-09 08:50:56 +0100

    Helps if I get the files in the right bloody place!

---
+++ src/TwwSaml/Module.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace TwwSaml;
+
+class Module
+{
+
+    public function onBootstrap(MvcEvent $e)
+    {
+        define("TOOLKIT_PATH", __DIR__ . '/../../vendor/onelogin/php-saml/');
+        require_once(TOOLKIT_PATH . '_toolkit_loader.php');
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\ClassMapAutoloader' => array(
+                    __DIR__ . '/autoload_classmap.php'
+            ),
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                // 'TwwSaml\AuthUri'         => 'TwwSaml\Factory\AuthUriFactory',
+            ),
+        );
+    }
+
+}
